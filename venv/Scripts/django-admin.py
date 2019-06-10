9c5d5a284558410ad919453f7aa3aeda2c668fe4
Fernando Cabello <amigo2@hotmail.com>
2019-06-10 22:12:43 +0200

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\fernando\desktop\fer_web_2019\fer_3d_django_blog\venv\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
