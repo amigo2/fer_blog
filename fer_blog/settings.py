69c4860aece00fbac12a24e5fd8bdafb343d4d87
Fernando Cabello <amigo2@hotmail.com>
2019-06-11 02:20:31 +0200

    3d working with django integration

--- fer_blog/settings.py
+++ fer_blog/settings.py
@@ -121,6 +121,6 @@ USE_TZ = True
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS - [
+STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'fer_blog/static')
 ]
