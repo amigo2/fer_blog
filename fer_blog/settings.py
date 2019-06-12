565bda5c90253cbcac0f0fd6da0a32430e2642a6
Fernando Cabello <amigo2@hotmail.com>
2019-06-12 20:57:13 +0200

    migrations

--- fer_blog/settings.py
+++ fer_blog/settings.py
@@ -76,8 +76,11 @@ WSGI_APPLICATION = 'fer_blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':   'fer_blog',
+        'USER':  'postgres',
+        'PASSWORD': '28489487P',
+        'HOST': 'localhost'
     }
 }
 
---
+++ run_the_matrix.py
@@ -0,0 +1,15 @@
+import subprocess
+import tkinter as tk
+
+#root= tk.Tk() 
+   
+#canvas1 = tk.Canvas(root, width = 350, height = 250) 
+#canvas1.pack()
+
+def start_batch(): 
+       subprocess.call([r'C:\Users\Fernando\Desktop\FER_WEB_2019\Fer_3D_django_blog\venv\Scripts\activate.bat'])
+           
+#button1 = tk.Button (root, text='Run The Matrix ',command=start_batch)
+#canvas1.create_window(170, 130, window=button1)
+
+#root.mainloop()
