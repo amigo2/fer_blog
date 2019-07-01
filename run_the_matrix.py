fa71bfeccdab7013f8b7cdb38aea2818996e45ab
Fernando Cabello <amigo2@hotmail.com>
2019-07-01 02:37:38 +0200

    commit test

--- run_the_matrix.py
+++ run_the_matrix.py
@@ -5,6 +5,7 @@ import tkinter as tk
    
 #canvas1 = tk.Canvas(root, width = 350, height = 250) 
 #canvas1.pack()
+#canvas1.pack()
 
 def start_batch(): 
        subprocess.call([r'.\venv\Scripts\activate'])
